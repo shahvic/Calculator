96b51da5a93c867ecf5e40f7f973c8295288d7b7
shahvic <viraj.shah@novartis.com>
2014-12-11 16:33:42 +0530

    First Commit
    First Commit

---
+++ src/com/calc/CalculatorDemo.java
@@ -0,0 +1,14 @@
+package com.calc;
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Functions f= new Functions();
+		System.out.println(f.add(1, 2));
+		System.out.println(f.sub(2, 1));
+		
+	}
+
+}
---
+++ src/com/calc/Functions.java
@@ -0,0 +1,11 @@
+package com.calc;
+
+public class Functions {
+
+	public int add(int a, int b){
+		return a+b;
+	}
+	public int sub(int a, int b){
+		return a-b;
+	}
+}
