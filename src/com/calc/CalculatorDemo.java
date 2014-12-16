d0784444bd90a20e04f082fcddd44196194b79a8
shahvic <viraj.shah@novartis.com>
2014-12-16 16:07:21 +0530

    Server commit
    Server commit

--- src/com/calc/CalculatorDemo.java
+++ src/com/calc/CalculatorDemo.java
@@ -8,6 +8,7 @@ public class CalculatorDemo {
 		Functions f= new Functions();
 		System.out.println(f.add(1, 2));
 		System.out.println(f.sub(2, 1));
+		System.out.println("Demo Ends-Server commit");
 		
 	}
 
