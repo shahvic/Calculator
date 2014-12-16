bdde3289d8be05e80bc88807c7192809bec943d3
shahvic <viraj.shah@novartis.com>
2014-12-16 15:59:10 +0530

    Demo Start
    Demo Start

--- src/com/calc/CalculatorDemo.java
+++ src/com/calc/CalculatorDemo.java
@@ -4,7 +4,7 @@ public class CalculatorDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Demo Starts");
 		Functions f= new Functions();
 		System.out.println(f.add(1, 2));
 		System.out.println(f.sub(2, 1));
