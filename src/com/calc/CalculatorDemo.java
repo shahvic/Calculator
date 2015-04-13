8233b060115ca71dfdc53afcba17457676625aa8
shahvic <viraj.shah@novartis.com>
2015-04-13 14:23:01 +0530

    Revert "Commit for Git change"
    This reverts commit 9581f25ea52dd03d269b370ecf3360f7bf486f87.

--- src/com/calc/CalculatorDemo.java
+++ src/com/calc/CalculatorDemo.java
@@ -15,7 +15,6 @@ public class CalculatorDemo {
 		System.out.println("User 1 changes");
 System.out.println("User 3 changes");
 System.out.println("User 4 changes");
-System.out.println("User 5 changes");
 		
 	}
 
